196e42344c546ee9ff9b6405bd46c9883ed4a379
Sneha Srivastav <srivastav.sneha04@gmail.com>
2026-08-09 03:08:57 +0530

    connected MongoDB database

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB!'))
+  .catch((err) => console.log('Connection failed:', err));
 
 app.get('/', (req, res) => {
   res.send('Hello, Healthcare Booking!');
